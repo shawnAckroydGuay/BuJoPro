8857554a7346b29f519428f0dd482c7af92c099c
Shawn <shawn@MacBook-Pro-de-Shawn.local>
2023-06-15 13:55:43 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Aspose.Pdf;
+
+Console.WriteLine("Hello, World!");
+
+
+TeXLoadOptions options = new TeXLoadOptions();
+
+Aspose.Pdf.Document pdfDocument = new Aspose.Pdf.Document("LaTex/monthPlanning.tex", options);
+
+pdfDocument.Save("LaTeX-to-PDF.pdf");
