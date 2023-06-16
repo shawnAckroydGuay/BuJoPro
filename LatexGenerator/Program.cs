6032472308d26914345078a7da28c771ec90ef75
Shawn David Guay <shawndavid95@gmail.com>
2023-06-16 14:21:21 -0400

    Modifier le programme afin qu'il génère un pdf

---
+++ LatexGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var latexPath = "C:\\Users\\shawn\\source\\repos\\LatexGenerator\\LatexGenerator\\LaTex";
+
+var pdfCreator = new WM.LaTex.PdfCreator();
+
+var monthPlanningPdfBytes = pdfCreator.Erzeuge(latexPath, "monthPlanning");
+
+var outputDocument = new PdfDocument();
+MemoryStream stream = new MemoryStream(monthPlanningPdfBytes);
+
+var inputPdfDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+outputDocument.AddPage(inputPdfDocument.Pages[0]);
+outputDocument.AddPage(inputPdfDocument.Pages[0]);
+
+outputDocument.Save(latexPath + "\\monthPlanning.pdf");
+File.Delete(latexPath + "\\monthPlanning.log");
+File.Delete(latexPath + "\\monthPlanning.aux");
+
