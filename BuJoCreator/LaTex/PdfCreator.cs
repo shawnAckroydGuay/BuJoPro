9921df786ef5a45c1cf4171187540b36369f472a
Shawn David Guay <shawndavid95@gmail.com>
2023-06-16 14:34:30 -0400

    Renommer le projet

---
+++ BuJoCreator/LaTex/PdfCreator.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace WM.LaTex
+{
+    public class PdfCreator
+    {
+        private byte[] _pdf;
+
+        public PdfCreator(string tmpPath = "/tmp") => this.TmpPath = tmpPath;
+
+        public string TmpPath { get; }
+
+        public byte[] Erzeuge(string filePath, string fileName)
+        {
+            if (this._pdf == null) 
+            {
+                using (Process process = new Process())
+                {
+                    process.EnableRaisingEvents = false;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.FileName = "pdflatex";
+                    ProcessStartInfo startInfo = process.StartInfo;
+                    DefaultInterpolatedStringHandler interpolatedStringHandler = new DefaultInterpolatedStringHandler(61, 2);
+                    interpolatedStringHandler.AppendLiteral(" -output-directory=");
+                    interpolatedStringHandler.AppendFormatted(filePath);
+                    interpolatedStringHandler.AppendLiteral(" -interaction=batchmode -shell-escape ");
+                    interpolatedStringHandler.AppendFormatted(filePath);
+                    interpolatedStringHandler.AppendLiteral("\\"+ fileName + ".tex");
+                    string stringAndClear = interpolatedStringHandler.ToStringAndClear();
+                    startInfo.Arguments = stringAndClear;
+                    for (int index = 0; index < 2; ++index)
+                    {
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                try
+                {
+                    this._pdf = File.ReadAllBytes(filePath + "\\" + fileName + ".pdf");
+                }
+                catch
+                {
+                }
+                File.Delete(filePath + "\\monthPlanning.log");
+                File.Delete(filePath + "\\monthPlanning.aux");
+            }
+            return this._pdf;
+        }
+    }
+}
---
+++ BuJoCreator/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var latexPath = "C:\\Users\\shawn\\source\\repos\\productivityAgendaCreator\\BuJoCreator\\LaTex";
+
+var pdfCreator = new WM.LaTex.PdfCreator();
+
+var monthPlanningPdfBytes = pdfCreator.Erzeuge(latexPath, "monthPlanning");
+
+var outputDocument = new PdfDocument();
+MemoryStream stream = new MemoryStream(monthPlanningPdfBytes);
+
+var inputPdfDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+outputDocument.AddPage(inputPdfDocument.Pages[0]);
+outputDocument.AddPage(inputPdfDocument.Pages[0]);
+
+outputDocument.Save(latexPath + "\\monthPlanning.pdf");
+
+
