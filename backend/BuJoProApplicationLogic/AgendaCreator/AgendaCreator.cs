dbc4d5e678a773975a9726878353353c839bc50d
Shawn Guay <shawnguay@Shawns-MBP.hitronhub.home>
2025-02-16 15:49:49 -0500

    Ajouter une page de planning annuelle et importer une image de couverture

--- backend/BuJoProApplicationLogic/AgendaCreator/AgendaCreator.cs
+++ backend/BuJoProApplicationLogic/AgendaCreator/AgendaCreator.cs
@@ -18,6 +18,8 @@ namespace BuJoProApplicationLogic.BuJoCreator
         private readonly byte[] _splittedDotedPaper;
         private readonly byte[] _blankPaper;
         public string MonthTemplatePath = AppContext.BaseDirectory + "/AgendaCreator/Alpaga/AlpagaCalendrierHL.tex";
+        public string AnnualPlanningPath = AppContext.BaseDirectory + "/AgendaCreator/Alpaga/PlanificationAnnuelleZigZag.tex";
+        public string CouverturePath = AppContext.BaseDirectory + "/AgendaCreator/Alpaga/CouvertureHL.tex";
         private readonly LatexToPdf _pdfCreator;
 
         public AgendaCreator()
@@ -28,43 +30,118 @@ namespace BuJoProApplicationLogic.BuJoCreator
             _blankPaper = File.ReadAllBytes(TemplatePath + "BlankHL.pdf");
         }
 
-        public byte[] CreerLePlanificateurEnPdf(int premierMois, int nombreDeMoisVoulu = 6)
+        public byte[] CreerLePlanificateurEnPdf(int premierMois,
+         string titre, 
+         byte[]? imageCouverture,
+         int nombreDeMoisVoulu = 6)
         {
+            if(imageCouverture is not null)
+                EnregistrerImageCouverture(imageCouverture);
+
             var listeDesMois = new List<DateTime>();
 
             for (int i = 0; i < nombreDeMoisVoulu; i++)
             {
                 //Ici les calculs servent à gérer si l'utilisateur demande 6 mois et que le premier
                 //mois est par exemple septembre(09): 09 10 11 12 01 02 03
-                int mois = (premierMois -1 + i ) % 12 + 1;
+                int mois = (premierMois - 1 + i) % 12 + 1;
                 int anneeDuMois = DateTime.Now.Year + (premierMois - 1 + i) / 12;
                 var moisEnFormatDateTime = new DateTime(anneeDuMois, mois, 1);
                 listeDesMois.Add(moisEnFormatDateTime);
             }
 
-            var pageAImprimer = new List<byte[]>
-            {
-                _blankPaper, // première page blanche
-                _dotedPaper, //deuxième page en pointillés
-                _dotedPaper //troisième page en pointillés
-            };
-            
+            var pagesAImprimer = new List<byte[]>();
+
+            var couverture = ObtenirCouverture(titre);
+            pagesAImprimer.Add(couverture);
+            pagesAImprimer.Add(_splittedDotedPaper);
+            pagesAImprimer.Add(_splittedDotedPaper);
+
+            var pagesPlannings = CreerPagesPlanningAnnuelle(premierMois);
+            pagesAImprimer.Add(pagesPlannings.Item1);
+            pagesAImprimer.Add(pagesPlannings.Item2);
+
+
             foreach (var month in listeDesMois)
             {
                 var calendrierLatexFormatte = CreerLaPageCalendrierDuMois(month.Year, month.Month);
                 File.WriteAllText(TemplatePath + "alpagaV2.tex", calendrierLatexFormatte);
                 var alpagaBytes = _pdfCreator.Convert(TemplatePath, "alpagaV2");
-                pageAImprimer.Add(alpagaBytes);
-                pageAImprimer.Add(_splittedDotedPaper);
-                pageAImprimer.Add(_splittedDotedPaper);
-                pageAImprimer.Add(_splittedDotedPaper);
+                pagesAImprimer.Add(alpagaBytes);
+                pagesAImprimer.Add(_splittedDotedPaper);
+                pagesAImprimer.Add(_splittedDotedPaper);
+                pagesAImprimer.Add(_splittedDotedPaper);
             }
 
-            pageAImprimer.Add(_blankPaper);
-            return CreerAgendaPDFPourPlierFormatLettre(pageAImprimer);
+            pagesAImprimer.Add(_blankPaper);
+            return CreerAgendaPDFPourPlierFormatLettre(pagesAImprimer);
 
         }
 
+        public void EnregistrerImageCouverture(byte[] imageCouverture){
+            var extension = imageCouverture.GetImageExtension();
+            File.WriteAllBytes(TemplatePath + "/imageCouverture" + extension, imageCouverture);
+        }
+
+        public byte[] ObtenirCouverture(string titre){
+            var couverture = File.ReadAllText(CouverturePath);
+            couverture = couverture
+                .Replace("titreARemplacer", titre);
+            File.WriteAllText(TemplatePath + "cover.tex", couverture);
+            return _pdfCreator.Convert(TemplatePath, "cover");
+        }
+
+        public (byte[], byte[]) CreerPagesPlanningAnnuelle(int premierMois)
+        {
+
+            var listeDesMois = new List<DateTime>();
+            for (int i = 0; i < 12; i++)
+            {
+                //Ici les calculs servent à gérer si l'utilisateur demande 6 mois et que le premier
+                //mois est par exemple septembre(09): 09 10 11 12 01 02 03
+                int mois = (premierMois - 1 + i) % 12 + 1;
+                int anneeDuMois = DateTime.Now.Year + (premierMois - 1 + i) / 12;
+                var moisEnFormatDateTime = new DateTime(anneeDuMois, mois, 1);
+                listeDesMois.Add(moisEnFormatDateTime);
+            }
+
+            var dic = new Dictionary<int, string>{
+                {0, "firstMonth"},
+                {1, "secondMonth"},
+                {2, "thirdMonth"},
+                {3, "fourthMonth"},
+                {4, "fifthMonth"},
+                {5, "sixthMonth"},
+            };
+
+
+            var definitionVariablesLatexPage1 = string.Empty;
+            var definitionVariablesLatexPage2 = string.Empty;
+            for (int i = 0; i < listeDesMois.Count; i++)
+            {
+                if (i < 6)
+                    definitionVariablesLatexPage1 += CreerLaListeDesJoursDuMoisPlanning(
+                        listeDesMois[i].Year,
+                        listeDesMois[i].Month, dic[i]);
+                else
+                    definitionVariablesLatexPage2 += CreerLaListeDesJoursDuMoisPlanning(
+                        listeDesMois[i].Year,
+                        listeDesMois[i].Month, dic[i - 6]);
+            }
+
+            string planningAnnuelleLatex = File.ReadAllText(AnnualPlanningPath);
+            var planningAnnuelleLatexPage1 = planningAnnuelleLatex
+                .Replace("monthsDefinitionToReplace", definitionVariablesLatexPage1)
+                .Replace("\\def\\isFirstPage{}", "\\newbool{isFirstPage}");
+            var planningAnnuelleLatexPage2 = planningAnnuelleLatex
+                .Replace("monthsDefinitionToReplace", definitionVariablesLatexPage2);
+
+            File.WriteAllText(TemplatePath + "annualPlanningPage1.tex", planningAnnuelleLatexPage1);
+            File.WriteAllText(TemplatePath + "annualPlanningPage2.tex", planningAnnuelleLatexPage2);
+            var planningAnnuelPage1 = _pdfCreator.Convert(TemplatePath, "annualPlanningPage1");
+            var planningAnnuelPage2 = _pdfCreator.Convert(TemplatePath, "annualPlanningPage2");
+            return (planningAnnuelPage1, planningAnnuelPage2);
+        }
         /// <summary>
         /// Cette fonction va simplement prendre une liste ordonnéee de fichiers pdf au format HL (Half-Letter)
         /// et va les imprimer de manière à ce qu'on puisse faire un agenda en pliant les feuilles de format
@@ -128,7 +205,7 @@ namespace BuJoProApplicationLogic.BuJoCreator
                 return stream.ToArray();
             }
         }
-        
+
         public string CreerLaPageCalendrierDuMois(int annee, int mois)
         {
             string listeDesJoursFormattes = CreerLaListeDesJoursDuMois(annee, mois);
@@ -165,6 +242,40 @@ namespace BuJoProApplicationLogic.BuJoCreator
             {
                 joursDuMois += "XX,";
             }
+            return joursDuMois.Remove(joursDuMois.Length - 1) + "}";
+
+        }
+
+        public string CreerLaListeDesJoursDuMoisPlanning(int year, int month, string titre)
+        {
+            var frenchCulture = new CultureInfo("fr-FR");
+            var textInfo = new CultureInfo("fr-FR", false).TextInfo;
+            var nomDuMois = frenchCulture.DateTimeFormat.GetMonthName(month);
+            nomDuMois = textInfo.ToTitleCase(nomDuMois);
+            var joursDuMois = $"\\def\\{titre}Name{{{nomDuMois} {year}}}";
+            joursDuMois += $"\\def\\{titre}DayList{{L,M,M,J,V,S,D,";
+            var premierJour = new DateTime(year, month, 1);
+            //Lundi=1, Mardi=2, ..., Dimanche=7
+            var numeroJourDeSemaine = (int)premierJour.DayOfWeek;
+            //Logique pour mettre des X pour les jours vide dans le calendrier
+            //Ex: si mon mois commence un mercredi, alors lundi et mardi n'auront pas 
+            //de jour dans l'affichage du calendrier
+            for (int i = 1; i < numeroJourDeSemaine; i++)
+            {
+                joursDuMois += "XX,";
+            }
+
+            int nombreDeJourDansLeMois = DateTime.DaysInMonth(year, month);
+
+            for (int i = 1; i <= nombreDeJourDansLeMois; i++)
+            {
+                var jour = new DateTime(year, month, i);
+                string jourFormatte = jour.ToString("dd");
+                jourFormatte = textInfo.ToTitleCase(jourFormatte);
+                joursDuMois += $"{jourFormatte},";
+            }
+
+            //Enlever la dernière virgule
             return joursDuMois.Remove(joursDuMois.Length - 1) + "}";
 
         }
